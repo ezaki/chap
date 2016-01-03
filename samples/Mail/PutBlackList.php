8684f9bfc3c9a5e5ed6bfeff19626acf48a5253a
keika299 <keika299@gmail.com>
2016-01-04 02:53:34 +0900

    add mail service samples

---
+++ samples/Mail/PutBlackList.php
@@ -0,0 +1,18 @@
+<?php
+
+require dirname(__DIR__) . '/../vendor/autoload.php';
+
+use keika299\ConohaAPI\Conoha;
+
+$client = new Conoha(array(
+    'username'=>'ConoHa',
+    'password'=>'paSSword123456#$%',
+    'tenantId'=>'487727e3921d44e3bfe7ebb337bf085e'
+));
+
+$mailService = $client->mailService();
+
+echo json_encode($mailService->putBlackList('mail UUID', array(
+    'mail1@example.com',
+    'mail2@example.com'
+)));
